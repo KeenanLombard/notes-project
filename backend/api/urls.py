b441c93c7f8533d42b7f4b158579f75921d443c7
KeenanLombard <keenanlombardd2@gmail.com>
2024-04-24 15:59:36 +0200

    basic functionallity, delete fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.NoteListCreate.as_view(), name="note-list"),
-    path('notes/delete/<int:pk>', views.NoteDelete.as_view(), name="delete-note")
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name="delete-note")
 ]
